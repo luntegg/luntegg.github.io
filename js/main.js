d28ca0be53134cefbbcd19c253951c8c42621859
luntegg <lunteggo@gmail.com>
2017-06-28 01:19:29 +0300

    css fix

--- js/main.js
+++ js/main.js
@@ -67,47 +67,47 @@ $(function() {
     };
 
     var bindElements = function() {
-        $('#phone')
-            .keydown(function (e) {
-                var key = e.charCode || e.keyCode || 0;
-                $phone = $(this);
-
-                // Auto-format- do not expose the mask as the user begins to type
-                if (key !== 8 && key !== 9) {
-                    if ($phone.val().length === 4) {
-                        $phone.val($phone.val() + ')');
-                    }
-                    if ($phone.val().length === 5) {
-                        $phone.val($phone.val() + ' ');
-                    }
-                    if ($phone.val().length === 9) {
-                        $phone.val($phone.val() + '-');
-                    }
-                    if ($phone.val().length === 12) {
-                        $phone.val($phone.val() + '-');
-                    }
-                }
-
-                return (key == 91 || key == 8 || key == 9 || key == 46 || (key >= 48 && key <= 57) || (key >= 96 && key <= 105));
-            })
-            .bind('focus click', function () {
-                $phone = $(this);
-
-                if ($phone.val().length === 0) {
-                    $phone.val('(');
-                }
-                else {
-                    var val = $phone.val();
-                    $phone.val('').val(val);
-                }
-            })
-            .blur(function () {
-                $phone = $(this);
-
-                if ($phone.val() === '(') {
-                    $phone.val('');
-                }
-            });
+        // $('#phone')
+        //     .keydown(function (e) {
+        //         var key = e.charCode || e.keyCode || 0;
+        //         $phone = $(this);
+        //
+        //         // Auto-format- do not expose the mask as the user begins to type
+        //         if (key !== 8 && key !== 9) {
+        //             if ($phone.val().length === 4) {
+        //                 $phone.val($phone.val() + ')');
+        //             }
+        //             if ($phone.val().length === 5) {
+        //                 $phone.val($phone.val() + ' ');
+        //             }
+        //             if ($phone.val().length === 9) {
+        //                 $phone.val($phone.val() + '-');
+        //             }
+        //             if ($phone.val().length === 12) {
+        //                 $phone.val($phone.val() + '-');
+        //             }
+        //         }
+        //
+        //         return (key == 91 || key == 8 || key == 9 || key == 46 || (key >= 48 && key <= 57) || (key >= 96 && key <= 105));
+        //     })
+        //     .bind('focus click', function () {
+        //         $phone = $(this);
+        //
+        //         if ($phone.val().length === 0) {
+        //             $phone.val('(');
+        //         }
+        //         else {
+        //             var val = $phone.val();
+        //             $phone.val('').val(val);
+        //         }
+        //     })
+        //     .blur(function () {
+        //         $phone = $(this);
+        //
+        //         if ($phone.val() === '(') {
+        //             $phone.val('');
+        //         }
+        //     });
 
         $(document).on('click', '.submit', function(e) {
             e.preventDefault();
